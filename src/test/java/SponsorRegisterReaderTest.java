c032a48d746c6e9a468233336d1dc20b14aeeda8
akshay <akms17@gmail.com>
2014-04-23 00:06:53 +0100

    Addition of gitignore and minor changes to directory structure

--- src/test/java/SponsorRegisterReaderTest.java
+++ src/test/java/SponsorRegisterReaderTest.java
@@ -1,24 +1,18 @@
-import java.io.File;
-
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.util.PDFTextStripper;
-import org.junit.Before;
 import org.junit.Test;
+
+import java.net.URL;
 
 public class SponsorRegisterReaderTest {
 
-	public static final String FILE_PATH = "data/Tiers_2___5_Register_of_Sponsors_2014-04-22.pdf";
+	public static final URL FILE_PATH = ClassLoader.getSystemResource("Tiers_2___5_Register_of_Sponsors_2014-04-22.pdf");
 	public static final String DOC_URL = "https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/305166/Tiers_2___5_Register_of_Sponsors_2014-04-22.pdf";
 
-	@Before
-	public void setUp() throws Exception {
-
-	}
-
-	@Test
+    @Test
 	public void testName() throws Exception {
 		PDFTextStripper stripper = new PDFTextStripper();
-		PDDocument load = PDDocument.load(new File(FILE_PATH));		
+		PDDocument load = PDDocument.load(FILE_PATH);
 		String text = stripper.getText(load);
 		//String text = stripper.getText(PDDocument.load(new URL(DOC_URL)));
 		stripper.setSpacingTolerance(0);
